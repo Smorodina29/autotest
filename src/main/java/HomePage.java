c1c7dc9fda954aa9e234b84a245ea8cd6bbc01a6
smorodina29 <miska156@mail.ru>
2018-05-02 12:27:47 +0300

    added HomePage

---
+++ src/main/java/HomePage.java
@@ -0,0 +1,6 @@
+import org.openqa.selenium.By;
+
+public class HomePage {
+    public static final By aboutAs = By.cssSelector("a[href*=\"about-us-en\"]");
+}
+
--- src/test/java/AboutAsTest.java
+++ src/test/java/AboutAsTest.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+
 import static java.lang.Thread.sleep;
 
 public class AboutAsTest {
