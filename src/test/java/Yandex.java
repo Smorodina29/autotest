bf86509443885e61a352d32169a05a26eac4a4e4
darya <darya.smorodina@ddgcorp.ru>
2018-05-03 15:11:28 +0300

    [+] added test for check dyson small

--- src/test/java/Yandex.java
+++ src/test/java/Yandex.java
@@ -3,31 +3,42 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.Set;
-import java.util.concurrent.TimeUnit;
-
-import static java.lang.Thread.sleep;
 
 public class Yandex {
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void start(){
+        driver = new ChromeDriver();
+    }
+
+    @AfterMethod
+    public void close(){
+        driver.close();
+    }
 
     @Test
     public void searchCleaner() throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
         driver.get("https://www.yandex.ru/");
+        String originalWindow = driver.getWindowHandle();
+        final Set<String> oldWindowsSet = driver.getWindowHandles();
+
         WebElement search = driver.findElement(By.name("text"));
         search.sendKeys("пылесос дайсон");
         driver.findElement(By.className("search2__button")).click();
-        final Set<String> oldWindowsSet = driver.getWindowHandles();
+
+
         driver.findElement(By.partialLinkText("dyson.com.ru")).click();
 
-        String newWindowHandle = (new WebDriverWait(driver, 10))
+        String newWindow = (new WebDriverWait(driver, 10))
                 .until(new ExpectedCondition<String>() {
                            public String apply(WebDriver driver) {
                                Set<String> newWindowsSet = driver.getWindowHandles();
@@ -37,12 +48,15 @@ public class Yandex {
                            }
                        }
                 );
-        //Boolean found = driver.findElement(By.id("ctl00_content_0_InnerCarousel_SlidesRepeater_ctl00_standardSlide_StandardSlideDiv"));
+
+        driver.switchTo().window(newWindow);
+
+        boolean isPresent = driver.findElements(By.id("ctl00_content_0_InnerCarousel_SlidesRepeater_ctl00_standardSlide_StandardSlideDiv")).size() > 0;
+        Assert.assertEquals(isPresent, true);
     }
 
     @Test
     public void TestDyson() throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
         driver.get("https://www.dyson.com.ru/");
         WebElement element = driver.findElement(By.id("ctl00_cphNavigation_topNavigation1_TopNavigationLinksRepeater_ctl02_TopNavigationLink"));
         Thread.sleep(3000);
